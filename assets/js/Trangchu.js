6471cf8be0b5dd03c79a1d7858988f12f43e8415
phamvinh1996 <160469653+phamvinh1996@users.noreply.github.com>
2024-10-22 15:25:38 +0700

    codecss

--- assets/js/Trangchu.js
+++ assets/js/Trangchu.js
@@ -57,12 +57,6 @@ $(document).ready(function () {
 });
 
 
-
-
-
-
-/*  */
-
   /*  */
   document.addEventListener('DOMContentLoaded', function() {
     var iconContainer = document.querySelector('.icon-container');
@@ -109,76 +103,9 @@ $(document).ready(function () {
     });
   
   /*  */
-    var prevScrollpos = window.pageYOffset;
-  var navbar = document.getElementById("navbar");
-  var toggleNav = document.getElementById("toggleNav");
-  
-  toggleNav.addEventListener("click", function() {
-    navbar.classList.toggle("hidden");
-  });
-  
-  window.onscroll = function() {
-    var currentScrollPos = window.pageYOffset;
-    if (prevScrollpos > currentScrollPos) {
-      navbar.classList.remove("hidden");
-    } else {
-      navbar.classList.add("hidden");
-    }
-    prevScrollpos = currentScrollPos;
-  }
-  /*  */
-  const cars = document.querySelectorAll('.car');
-  
-  function startAnimation() {
-    cars.forEach(car => {
-      car.style.animationPlayState = 'running';
-    });
-  }
-  
-  function stopAnimation() {
-    cars.forEach(car => {
-      car.style.animationPlayState = 'paused';
-    });
-  }
-  
-  /*  */
-  const carIcon = document.querySelector('.car-icon');
-  
-  function startMoving() {
-    carIcon.classList.add('move');
-  }
-  
-  function stopMoving() {
-    carIcon.classList.remove('move');
-  }
-  
-  // Gọi hàm startMoving để bắt đầu di chuyển
-  startMoving();
-  
+
   /*  */
   
-  /* chuyển động xe ô tô  foooter*/
-  
-  const car = document.querySelectorAll('.car');
-  
-  // Thiết lập thời gian chờ giữa mỗi lần di chuyển
-  const delay = 2; // Thời gian chờ giữa các di chuyển (đơn vị giây)
-  
-  // Thiết lập di chuyển cho từng ô tô
-  cars.forEach((car, index) => {
-    car.style.animationDelay = `${index * delay}s`;
-  });
-  
-  /* Dịch vụ nổi  bật */
-  $('.single-item-rtl').slick({
-    rtl: true
-  });
-  /* di chuyển trang dịch vụ nổi bât */
-   $('.multiple-items').slick({
-     infinite: true,
-    slidesToShow: 3,
-    slidesToScroll: 3
-   });
   /*  */
   $(document).ready(function () {
     $('.slider').slick({
